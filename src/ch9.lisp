a77e3bf4beebae8e882a7ef8ed25f6bf96f4b741
Sil Vaes <sil.g.vaes@gmail.com>
2025-04-24 21:12:06 +0200

    Finished chapter 9.

--- src/ch9.lisp
+++ src/ch9.lisp
@@ -53,10 +53,112 @@
   (format t "~&---------")
   (format t "~&~A | ~A | ~A" (or (nth 6 board) " ") (or (nth 7 board) " ") (or (nth 8 board) " "))
   nil)
-(print-board '(X O X nil nil nil O X O))
+;; (print-board '(X O X nil nil nil O X O))
 
 (defun read-square ()
   (format t "Please type in a number: ")
   (let ((x (read)))
     (format t "The numbers ~S squared is ~S.~%" x (* x x))))
-(read-square)
+'(read-square)
+
+(defun calc-wages ()
+  (format t "~&Hours worked: ")
+  (let ((hours (read)))
+    (format t "~&Wage per hour: ")
+    (let ((wages (read)))
+      (format t "~&Gross pay is ~A~&" (* hours wages)))))
+;; (calc-wages)
+
+(defun cookie-monster ()
+  "Reads input from terminal until the symbol COOKIE is encountered."
+  (format t "~%Feed me! Enter something: ")
+  (let ((input (read)))
+    (cond
+      ((eq input 'COOKIE)
+       (format t "~%Om nom nom nom... COOKIE! *munch* *munch*~%")
+       input)
+      (t
+       (format t "~%~A's not a cookie! Me want cookie!~%" input)
+       (cookie-monster)))))
+;; (cookie-monster)
+
+(defun riddle ()
+  (if (yes-or-no-p
+       "Do you seek enlightenment?")
+      (format t "Then do not ask for it!")
+      (format t "You have found it.")))
+;; (riddle)
+
+;; Read files with `with-open-file
+;; Special stream object in a `let` like context
+(defun get-tree-data ()
+  (with-open-file (stream "./src/timber.dat")
+    (let* ((tree-loc (read stream))
+           (tree-table (read stream))
+           (num-trees (read stream)))
+      (format t "~&There are ~S trees on ~S."
+              num-trees tree-loc)
+      (format t "~&They are ~S" tree-table))))
+;; (get-tree-data)
+;; Writing files
+(defun save-tree-data (tree-loc tree-table num-trees)
+  (with-open-file (stream "./src/timber.newdat"
+                          :direction :output)
+    (format stream "~S~%" tree-loc)
+    (format stream "~S~%" tree-table)
+    (format stream "~S~%" num-trees)))
+;; (save-tree-data "The West Ridge"
+;;                 '((45 redwood) (22 oak) (43 maple))
+;;                 110)
+;; (format t "a~S" 'b)
+;; (format t "always~%broke")
+;; (format t "~S~S" 'alpha 'bet)
+
+(defun space-over (n)
+  (cond ((zerop n) nil)
+        ((< n 0) (format t "Error!"))
+        (t (format t " ")
+           (space-over (1- n)))))
+(defun test-space-over (n)
+  (format t "~%>>>")
+  (space-over n)
+  (format t "<<<"))
+;; (test-space-over 10)
+
+(defun plot-one-point (plotting-string y-val)
+  (space-over y-val)
+  (format t "~A~%" plotting-string))
+(format t "~%")
+(plot-one-point "****" 4)
+
+(defun plot-points (plotting-string point-list)
+  (mapcar (lambda (elem) (plot-one-point plotting-string elem)) point-list))
+(plot-points "< >" '(4 6 8 10 8 6 4))
+
+(defun generate (lower upper)
+  (cond ((eq lower (1+ upper)) nil)
+        ((> lower upper) 'ERROR)
+        (t (cons lower (generate (1+ lower) upper)))))
+(generate -3 3)
+
+(defun make-graph (func start end plotting-string)
+  (plot-points plotting-string (mapcar func (generate start end))))
+(defun square (x)
+  (* x x))
+(make-graph #'square -14 14 "mahsa")
+
+(defun read-my-file ()
+  (with-open-file (stream "./src/timber.dat")
+    (let ((contents
+            (read-all-objects stream (list '$eof$))))
+      (format t "~&Read ~S object from the file."
+              (length contents))
+       contents)))
+(defun read-all-objects (stream eof-indicator)
+  (let ((result (read stream nil eof-indicator)))
+    (if (eq result eof-indicator)
+        nil
+        (cons result
+              (read-all-objects stream
+                                eof-indicator)))))
+(read-my-file)
